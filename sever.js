dcf7b010adf5447b2b748ebb9bf572701d9e4852
Korwynn Wagstaff <Korwynnwagstaff@gmail.com>
2017-10-19 18:58:18 -0500

    Final changes

--- sever.js
+++ sever.js
@@ -1,4 +1,3 @@
-// Pull in required dependencies
 var Express = require('express');
 var BodyParser = require('body-parser');
 var MethodOverride = require('method-override');
@@ -7,7 +6,7 @@ var port = process.env.PORT || 3000;
 
 var App = Express();
 
-App.use(Express.static(process.cwd() + '/app/public'));
+App.use(Express.static(process.cwd() + '/public'));
 App.use(BodyParser.urlencoded({ extended: false }));
 App.use(MethodOverride('_method'));
 
@@ -16,7 +15,7 @@ var exphbs = require('express-handlebars');
 App.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 App.set('view engine', 'handlebars');
 
-var Routes = require('./app/controllers/burgers_controller.js');
+var Routes = require('./controllers/burgers_controller.js');
 
 App.use('/', Routes);
 App.listen(port);
